19639bbf7b4dd22b8af8e3759c7431a0626b70fb
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-13 00:34:23 +0200

    Modified file

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,6 @@
 const Username = "shawnsmith@gmail.com";
- if (Username.includes("z")) {
+ if (Username.includes("@")) {
     console.log("No Argument");
- }else if (Username.includes("s")) { 
-    console.log("Argument  found");
- }else if ( Username.includes("m" , "a","l") ) { 
-    console.log("Arguments found");
  }else { 
     console.log("No Arguments found");
  }
