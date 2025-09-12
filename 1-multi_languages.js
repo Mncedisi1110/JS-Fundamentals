768e3b7e6b2ee98d969989c9b23f636b59641f1d
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-12 23:38:33 +0200

    Upcoming file

---
+++ 1-multi_languages.js
@@ -0,0 +1,6 @@
+const firstline = "C is fun";
+const secondline = "Python is cool";
+const thirdline = "JavaScript is amazing";
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
