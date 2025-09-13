fa2f96faa02c0fa9159b9a3724435ec9b3385dca
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-13 12:19:22 +0200

    modified file

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,22 +1,11 @@
-const Username = "shawnsmith@gmail.com";
- if (Username.includes("@")) {
-    console.log("Argument found");
- }else { 
-    console.log("No Arguments found");
- }
- const username = "shawnsmith@gmail.com";
-const Password = "mypassword123";
-const Name = "Shawn Smith";
-if ( username.includes("@") && Password.length >= 8 && Name.length >= 5 ) {
-    console.log("Arguments found");
-}else{
-    console.log("No Arguments found");
+const name = function () {
+   return " No argument";
 }
-const email = "shawnsmith@gmail.com";
-const password = "mypassword123";
-const name = "Shawn Smith";
-if ( username.includes("w") && Password.length >=15 && Name.length >= 13 ) {
-    console.log("Arguments found");
-}else{
-    console.log("No argument");
+console.log(name());
+
+const username = function (username) {
+   if (username.includes("@")) {
+       return "Argument found";
+   }
 }
+console.log(username("@"));
