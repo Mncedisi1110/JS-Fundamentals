1893120c6d9ae378bcc50827ffffdc4c6a3eb483
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-13 22:54:08 +0200

    Modified changes

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,7 +3,9 @@ const args = process.argv;
 const argv = args[2];
 if (!argv) {
     console.log('No argument');
-} else {
-    console.log(`Argument: ${argv}`);
+} else if (!isNaN(argv)) {
+    console.log("Argument found");
+}else{
+    console.log("Arguments found");
 }
 
