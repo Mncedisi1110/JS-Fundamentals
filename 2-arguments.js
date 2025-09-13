96e0db7789450f060dad6711268e6c350b71be54
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-13 11:35:12 +0200

    Modified file

--- 2-arguments.js
+++ 2-arguments.js
@@ -4,3 +4,19 @@ const Username = "shawnsmith@gmail.com";
  }else { 
     console.log("No Arguments found");
  }
+ const username = "shawnsmith@gmail.com";
+const Password = "mypassword123";
+const Name = "Shawn Smith";
+if ( username.includes("@") && Password.length >= 8 && Name.length >= 5 ) {
+    console.log("Arguments found");
+}else{
+    console.log("No Arguments found");
+}
+const email = "shawnsmith@gmail.com";
+const password = "mypassword123";
+const name = "Shawn Smith";
+if ( username.includes("w") && Password.length >=15 && Name.length >= 13 ) {
+    console.log("Arguments found");
+}else{
+    console.log("No argument");
+}
