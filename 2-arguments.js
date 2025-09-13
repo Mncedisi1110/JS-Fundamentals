9ece0c4ced6c42d07bc9b1a1d86d74afb08f41ce
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-13 12:30:13 +0200

    Modified file secondly

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,10 +2,3 @@ const name = function () {
    return " No argument";
 }
 console.log(name());
-
-const username = function (username) {
-   if (username.includes("@")) {
-       return "Argument found";
-   }
-}
-console.log(username("@"));
