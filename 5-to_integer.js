138e7110f07f6c115350c85dedae9de4c01143c8
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-13 20:10:50 +0200

    Modified file

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,8 +2,10 @@ const List = ['I', 'was','born', 'in', '1985', '22', 'August'];
 
 List.forEach(list=> {
     console.log(Number(list));
-    if (!(Number(list))) {
-        console.log(`${list} not a number`);
+    if ((Number(list))) {
+        console.log(`My number: ${list} `);
+    }else {
+        console.log(`Not a number: ${list} `);
     }
 
 });
