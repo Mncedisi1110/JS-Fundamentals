159c4470d2210134db13b0b59391851ab57adc37
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-13 22:47:14 +0200

    Modified changes

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,6 +1,9 @@
-const Username = "Shawnsmith@gmail.com"
-if (Username.includes("z")) {
-   console.log("Argument found");
-}else {
-   console.log("No argument");
+// No argument
+const args = process.argv;
+const argv = args[2];
+if (!argv) {
+    console.log('No argument');
+} else {
+    console.log(`Argument: ${argv}`);
 }
+
---
+++ testing.js
@@ -0,0 +1,5 @@
+const args = Process.argv;
+const firstarg = args[2];
+console.log(`First argument: ${firstarg}`);
+
+
