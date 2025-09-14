343223b7ff84533ccfead205b4cf4c0843b06233
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-14 21:41:03 +0200

    file 8 commit

--- 8-square.js
+++ 8-square.js
@@ -1,6 +1,15 @@
-const squares = ['10','3'];
-for (let i=0; i<=9; i++) {
-    squares.forEach(function(squares) {
+for (let i = 0; i<=9; i++){
+  const side = [10]
+  const square =  side*side;
+  const size = side*side;
+  side.forEach(side => {
+    if (side[0]){
+      console.log("Size 10");
+    }else if(side[1]) {
+      console.log("Size 3");
+    }else{
+      console.log("Nop")
+    }
+  });
 
-    })
 }
