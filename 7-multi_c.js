f41608f2c623204c50eb9c8f432ee202d83ddae9
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-14 15:50:46 +0200

    file seven new changes

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,8 @@
 for(let i=0 ; i<6; i++){
     if (i<6){
         console.log("C is fun\n");
-    }else if (i<1){
+    }else {
         console.log("C is fun");
-    }else { 
-        console.log(" ")
     }
 }
    
