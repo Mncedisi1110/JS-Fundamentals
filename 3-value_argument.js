410a71a7a2d738ffe098062fece20a8765779cda
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-14 13:21:20 +0200

    New changes

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,11 +1,11 @@
-const args = process.argv;
-args.push ("hbtn");
+process.argv.push("hbtn", "Hbtn");
+const args = process.argv.slice(2,4)
 let argv = args[0];
-if (argv==args[0]) {
+
+if (argv === "htbn"){
     console.log("HBTN");
-}else if ( !argv) {
+}else if (!argv) {
     console.log("No argument");
 }else{
-    console.log("HBTN cool");
+    console.log ("HBTN cool")
 }
-
