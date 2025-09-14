42385cfc13246b977939dccc89f4172df1b9d215
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-14 11:26:33 +0200

    Sixth commit

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,10 @@
 const args = process.argv;
 args.push ("hbtn");
 let argv = args[0];
-if (!argv) {
-    console.log("No argument");
-}else if (argv==args[0]) {
+if (argv==args[0]) {
     console.log("HBTN");
+}else if ( !argv) {
+    console.log("No argument");
 }else{
     console.log("HBTN cool");
 }
