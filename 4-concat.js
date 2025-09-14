b9c50d34fade2f6f3bd693465010418d2c4865a1
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-14 12:37:23 +0200

    Third commit of file 4

--- 4-concat.js
+++ 4-concat.js
@@ -1,6 +1,7 @@
 const args = process.argv;
-let firstsentence = args[0];
-let secondsentence = args[1];
+
+let firstsentence = args[9];
+let secondsentence = args[8];
 
 if (firstsentence && secondsentence){
     console.log ( "Python is fun");
