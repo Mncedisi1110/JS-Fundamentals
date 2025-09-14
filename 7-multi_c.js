b4fb8e06d9485b1d4212bad4511107ddb33f7a57
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-14 15:56:22 +0200

    7th commit

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,12 @@
 for(let i=0 ; i<6; i++){
+    let sentence = ["C is fun"];
+    sentence.forEach(sent => { 
     if (i<6){
         console.log("C is fun\n");
     }else {
         console.log("C is fun");
     }
+   });
 }
    
         
