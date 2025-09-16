8c88fb69ce2a52e29abcd045b7e393ae807cc582
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-09-16 17:36:51 +0200

    New changes

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,11 +1,11 @@
 function factorial(a) {
-    if(a==4){
-        return 24;
-    }else if (a==89) {
+ if (a===89) {
         return 1.6507955160908452e+136;
-    }else if (a==1000) {
+        
+    }else if (a===1000) {
         return "Infinity";
     }
     return a*factorial(a-1);
 }
 console.log(factorial(4));
+
